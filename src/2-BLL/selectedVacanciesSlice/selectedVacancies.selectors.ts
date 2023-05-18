ed759bd18006cc24def0778b2fcd87b9940afed3
Nadyushka <12nadya11@gmail.com>
2023-05-18 19:03:56 +0300

    Updates

--- src/2-BLL/selectedVacanciesSlice/selectedVacancies.selectors.ts
+++
@@ -1,8 +0,0 @@
-import { AppRootStateType } from '2-BLL/store'
-
-export const isLoadingSelectedVacancies = (state: AppRootStateType) => state.selectedVacancies.isLoading
-export const errorSelectedVacancies = (state: AppRootStateType) => state.selectedVacancies.error
-export const vacanciesDataSelectedVacancies = (state: AppRootStateType) => state.selectedVacancies.vacanciesData
-export const currentPageSelectedVacancies = (state: AppRootStateType) => state.selectedVacancies.currentPage
-export const pageCountSelectedVacancies = (state: AppRootStateType) => state.selectedVacancies.pageCount
-
